e91d872f30e81aca79882a44f96ccede484e841d
eduardogrilo <jose_egrl@hotmail.com>
2022-01-09 12:23:27 +0000

    getTotalSpent function has a base structure

--- santaSleighMakeover.java
+++ santaSleighMakeover.java
@@ -2,11 +2,11 @@ public class santaSleighMakeover {
 
     public static int calculateAverage(int[] numArray) {
         
-        int index = 0, sum = 0;
+        int i = 0, sum = 0;
 
-        while (index < numArray.length) {
-            sum += numArray[index];
-            index++;
+        while (i < numArray.length) {
+            sum += numArray[i];
+            i++;
         }
          
         return (sum / numArray.length);
@@ -29,32 +29,49 @@ public class santaSleighMakeover {
 
     public int getTotalSpent(int[] runners, int[] holders, int budget) {
         
+        int result1 = 0, result2 = 0;
+
         if (calculateSD(runners) > calculateSD(holders)) {
-            for (int runner : runners) {
-                if (runner > calculateAverage(runners)) {
+            int runner = 0;
+            for (int i : runners) {
+                if (i > calculateAverage(runners)) {
+                    runner = i;
                     break;
                 }
+            }
+            for (int holder=holders.length-1; holder>=0; holder--) {
+                if ((holder + runner) <= budget){
+                    result1 = holder + runner;
+                }            
             }
             
         } else {
-            for (int holder : holders) {
-                if (holder > calculateAverage(holders)) {
+            int holder = 0;
+            for (int i : holders) {
+                if (i > calculateAverage(holders)) {
+                    holder = i;
                     break;
                 }
             }
-            
+            for (int runner=runners.length-1; runner>=0; runner--) {
+                if ((holder + runner) <= budget){
+                    result2 = holder + runner;
+                }            
+            }           
         }
-        return -1;
+        if(result1 == 0 && result2 == 0){
+            return -1;
+        } else if (result1 > result2){
+            return result1;
+        } else {
+            return result2;
+        }
     }
     //Driver Code 
     public static void main(String[] args) {
           
         int[] runners = {60, 80, 100};
         int[] holders = {17, 19, 21};
-        System.out.println(calculateAverage(runners));
-        System.out.println(calculateAverage(holders));
-        System.out.println(calculateSD(runners));
-        System.out.println(calculateSD(holders));
-
+        
     }
 }
