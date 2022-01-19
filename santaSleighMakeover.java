9af686adccbc7f156abe1b89ba3a469067fcfa8a
koldramke <jose_egrl@hotmail.com>
2022-01-19 19:13:33 +0000

    Deleted launch.json file

--- santaSleighMakeover.java
+++ santaSleighMakeover.java
@@ -1,43 +1,27 @@
 public class santaSleighMakeover {
 
-    public int calculateAverage(int[] numArray) {
-
-        int i = 0, sum = 0;
-        while (i < numArray.length) {
-            sum += numArray[i];
-            i++;
-        }
-        return sum / numArray.length;
-    }
-
     public int getTotalSpent(int[] runners, int[] holders, int budget) {
 
-        int result1 = 0, runner = 0, holder = 0, runnersLengh = runners.length;
-        while (result1 == 0) {
-            for (int i0 = runnersLengh - 1; i0 >= 0; i0--) {
-                if (runners[i0] <= budget) { // Check if the budget is enough
-                    if (runners[i0] >= calculateAverage(runners)){ // Check if the average value is higher than the budget
-                        runner = runners[i0];
-                    } else {
-                        for (int i1 = 0; i1 >= 0; i1++) {
-                            if (runners[i1] > calculateAverage(runners)) {
-                                runner = runners[i1];
-                            }
-                        }
-                    }
-
+        int max_sum = 0;
+        for (int index_holder : holders) {
+            for (int index_runners : runners) {
+                if (((index_holder + index_runners) <= budget) && (index_holder + index_runners) > max_sum) {
+                    max_sum = (index_holder + index_runners);
                 }
-                result1 = 0;
-
             }
         }
 
+        if (max_sum > 0) {
+            return max_sum;
+        }
         return -1;
     }
 
     // Driver Code
     public static void main(String[] args) {
 
+        // int[] runners = { 50, 70, 100, 989, 4000 };
+        // int[] holders = { 5, 56, 250, 560, 400 };
         int[] runners = { 50, 70, 100, 989, 4000 };
         int[] holders = { 5, 56, 250, 560, 400 };
         int budget = 350;
